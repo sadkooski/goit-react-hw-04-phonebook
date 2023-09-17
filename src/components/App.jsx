c71e616a132af4f96abc54393e3bbde3b1f4c9e7
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-09-17 17:24:32 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,37 @@
-import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
+import React, { useState, useEffect } from 'react';
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-    this.state = {
-      contacts: [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ],
-      filter: '',
-      name: '',
-      number: '',
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     const storedContacts = JSON.parse(localStorage.getItem('contacts'));
     if (storedContacts) {
-      this.setState({ contacts: storedContacts });
+      setContacts(storedContacts);
     }
-  }
+  }, []);
 
-  componentDidUpdate() {
-    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  handleSubmit = evt => {
+  const handleSubmit = evt => {
     evt.preventDefault();
-
-    const { contacts } = this.state;
     const form = evt.currentTarget;
-    const name = form.elements.name.value;
-    const number = form.elements.number.value;
-
-    const existingContact = contacts.find(contact => contact.name === name);
+    const newName = form.elements.name.value;
+    const newNumber = form.elements.number.value;
+    const existingContact = contacts.find(contact => contact.name === newName);
 
     if (existingContact) {
       alert(`${name} is already in contacts.`);
@@ -48,69 +39,62 @@ class App extends Component {
     }
 
     const newContact = {
-      name: name,
+      name: newName,
       id: nanoid(),
-      number: number,
+      number: newNumber,
     };
 
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
-
+    setContacts(prevContacts => [...prevContacts, newContact]);
+    setName('');
+    setNumber('');
     form.reset();
   };
 
-  handleBrowser = evt => {
+  const handleBrowser = evt => {
     const filterValue = evt.target.value.toLowerCase();
-    this.setState({ filter: filterValue });
+    setFilter(filterValue);
   };
 
-  handleDelete = evt => {
+  const handleDelete = evt => {
     const button = evt.currentTarget;
     const name = button.name;
-    const { contacts } = this.state;
     const newContactsArr = contacts.filter(contact => contact.name !== name);
 
-    this.setState({
-      contacts: newContactsArr,
-    });
+    setContacts(newContactsArr);
   };
 
-  render() {
-    const { contacts, filter } = this.state;
-    const filteredContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter)
-    );
-    const contactsStatus = filteredContacts.length;
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter)
+  );
+  const contactsStatus = filteredContacts.length;
 
-    return (
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: '100vh',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <h1>Phonebook</h1>
-        <ContactForm handler={this.handleSubmit} />
-        <h2>Contacts</h2>
-        <Filter handler={this.handleBrowser} />
-        {contactsStatus > 0 &&
-          filteredContacts.map(contact => (
-            <ContactList
-              key={contact.id}
-              number={contact.number}
-              name={contact.name}
-              handler={this.handleDelete}
-            />
-          ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <ContactForm handler={handleSubmit} />
+      <h2>Contacts</h2>
+      <Filter handler={handleBrowser} />
+      {contactsStatus > 0 &&
+        filteredContacts.map(contact => (
+          <ContactList
+            key={contact.id}
+            number={contact.number}
+            name={contact.name}
+            handler={handleDelete}
+          />
+        ))}
+    </div>
+  );
+};
 
 export default App;
